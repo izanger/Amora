8de5b49a8d4de354b1d8e2e75fb8b9035a5c157a
Zach Johnson <john1861@purdue.edu>
2018-02-07 20:17:06 -0500

    Refine login

--- amora/src/Login.js
+++ amora/src/Login.js
@@ -3,6 +3,9 @@ import {auth, google} from "./rebase.js"
 import {Row, Grid, Col} from 'react-bootstrap'
 
 import amoraLogo from "./images/amora_logo.png"
+import googleLoginNormal from "./images/btn_google_signin_light_normal_web@2x.png"
+import googleLoginPressed from "./images/btn_google_signin_dark_pressed_web@2x.png"
+import googleLoginHover from "./images/btn_google_signin_light_focus_web@2x.png"
 import "./Login.css"
 
 class Login extends Component {
@@ -27,9 +30,10 @@ class Login extends Component {
                 <Col xs={10} sm={6} md={6}>
                     <center><img className="amora" src={amoraLogo} /></center>
                       <div className="subtitle">Teamwork Makes the Dream Work</div>
-                      <button className="signin-button" onClick={this.signin}>
+                      {/*<button className="signin-button" onClick={this.signin}>
                           Sign in with Google
-                      </button>
+                      </button>*/}
+                      <img onClick={this.signin} className="googleLogin" src={googleLoginNormal}></img>
                 </Col>
                 <Col xs={1} sm={3} md={3}>
 
