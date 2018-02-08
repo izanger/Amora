57912aa498e08e5b34f6a809b3ebe7ed83e2bef2
Zach Johnson <john1861@purdue.edu>
2018-02-08 00:37:36 -0500

    Make login appear in center of screen

--- amora/src/App.js
+++ amora/src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
-        <Home />
+        <Login />
       </div>
     );
   }
