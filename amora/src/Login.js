322245ee649dacc300838c887b90717df733cad7
Zach Johnson <john1861@purdue.edu>
2018-02-07 19:26:17 -0500

    update login

--- amora/src/Login.js
+++ amora/src/Login.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {auth, google} from "./rebase.js"
 import {Row, Grid, Col} from 'react-bootstrap'
 
-import amoraLogo from "./amora_logo_small.png"
+import amoraLogo from "./images/amora_logo.png"
 import "./Login.css"
 
 class Login extends Component {
@@ -20,23 +20,29 @@ class Login extends Component {
     render = () => {
         return (
             <Grid fluid className="login-grid">
-                <Col md={2}>
-                    
+              <Row className="login-row">
+                <Col xs={1} sm={3} md={3}>
+
                 </Col>
-                <Col md={8}>
-                    <img className="amora" src={amoraLogo} />
-                    <div className="subtitle">Teamwork makes the dream work</div>
-                    <button className="signin-button" onClick={this.signin}>
-                        Sign in with Google
-                    </button>
+                <Col xs={10} sm={6} md={6}>
+                    <center><img className="amora" src={amoraLogo} /></center>
+                      <div className="subtitle">Teamwork Makes the Dream Work</div>
+                      <button className="signin-button" onClick={this.signin}>
+                          Sign in with Google
+                      </button>
                 </Col>
-                <Col md={2}>
-                    
+                <Col xs={1} sm={3} md={3}>
+
                 </Col>
+              </Row>
+
+              <Row>
+
+              </Row>
             </Grid>
         )
     }
-    
+
 }
 
 export default Login;
