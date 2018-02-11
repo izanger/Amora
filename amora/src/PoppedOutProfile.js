3f6325d6fe9bd85b2ba15ed158adf67a209e3771
David Wood <wood154@purdue.edu>
2018-02-11 14:59:57 -0500

    Popup works correctly
    Button to open profile view exists with some temporary text in it.

--- amora/src/PoppedOutProfile.js
+++ amora/src/PoppedOutProfile.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react'
 import rebase, { auth, google} from "./rebase.js"
 import { Row, Grid, Col } from 'react-bootstrap'
+import ReactDOM from 'react-dom';
+//import Modal from 'react-responsive-modal';
+import 'react-responsive-modal/lib/react-responsive-modal.css';
+import Modal from 'react-responsive-modal/lib/css';
 
 import amoraLogo from "./images/amora_logo.png"
 
 class PoppedOutProfile extends React.Component {
-  constructor(props) { super(props) }
+  constructor() {
+    super();
+
+    this.state = { open: false }
+  }
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
 
   render() {
+    const { open } = this.state;
     return (
       <div>
-        {this.props.profile.map(msg =>
-          <div>
-            {item.date}
-            <pre>{msg.text}</pre>
-          </div>
-        )}
+        <button onClick={this.onOpenModal}>Open modal</button>
+        <Modal open={open} onClose={this.onCloseModal} little>
+          <h2>Simple centered modal</h2>
+        </Modal>
       </div>
-    )
+    );
   }
 }
+
+export default PoppedOutProfile;
