41b0353969402a033b138e0f2aad234d7e5a9926
Ben Kahlert <bkahlert@purdue.edu>
2018-02-14 17:44:21 -0500

    Functioning email validation process

--- amora/src/CreateProjectForm.js
+++ amora/src/CreateProjectForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import rebase from "./rebase";
 import InviteList from "./InviteList"
-import { emailRegistered, validateEmail } from "./apphelpers.js"
+import { emailRegistered, validateEmail, buildUserFromGoogle } from "./apphelpers.js"
 
 import "./CreateProjectForm.css"
 
@@ -39,20 +39,12 @@ class CreateProjectForm extends Component {
         }
     }
 
-    // General case method for inviting a user
-    inviteUser = () => {
-        const newState = { ...this.state }
-        if (this.state.inviteValue !== "") { //email was found in database
-            if (this.isEmailValid()) {
-                newState.inviteValue = "";
-                newState.userList.push(this.state.inviteValue)
-                this.setState(newState)
-            }
-         }
-    }
-
     // Checks to see is this.state.emailValue is valid
-    isEmailValid = () => {
+    emailValidationProcess = () => {
+        if (this.state.inviteValue === "") {
+            return false
+        }
+
         const newState = { ...this.state }
         if (!validateEmail(this.state.inviteValue)) {
             newState.errorValue = "Please enter a valid email address..."
@@ -60,23 +52,29 @@ class CreateProjectForm extends Component {
             return false
         }
 
-        // BEGIN PORTION NEEDS TO BE WORKED ON
         const promise = emailRegistered(this.state.inviteValue)
         promise.then((data) => {
-            const tempState = { ...this.state }
-            newState.promise = data.val()
+            if (!data.val()) {
+                newState.errorValue = "That email address has not been registered with Amora..."
+                this.setState(newState)
+                return false
+            }
+            if (this.state.userList.includes(this.state.inviteValue)) {
+                newState.errorValue = "You've already added that user to this project..."
+                this.setState(newState)
+                return false
+            }
+            newState.errorValue = ""
+            newState.inviteValue = "";
+            newState.userList.push(this.state.inviteValue)
             this.setState(newState)
+            return true
         })
-        if (!this.state.promise) {
-            newState.errorValue = "That email address has not been registered with Amora..."
-            this.setState(newState)
-            return false
-        }
-        // END PORTION NEEDS TO BE WORKED ON
-
-        newState.errorValue = ""
-        this.setState(newState)
-        return true
+        // TODO: 
+        // MAKE USER LIST HOLD USER OBJECTS
+        // MAKE IT SO USERS GET AN INVITE IN DATABASE
+        // WORK ON SYCINGSTATE WITH USERS SO THEIR INVITES WILL BE UPDATED ON THEIR CLIENTS AUTOMATICALLY
+        // MAKE INVITE PAGE / ACCEPTING INVTITE LOGIC
     }
 
     // Checks to see if project is valid or not
@@ -127,7 +125,7 @@ class CreateProjectForm extends Component {
                 <input type="text" placeholder="Email of person you'd like to invite" className="createProjectInput"
                 value={this.state.inviteValue} onChange={this.changeInviteValue} onKeyDown={this.enterInviteValue} />
                 <p className="errorBox">{this.state.errorValue}</p>
-                <button className="createProjectInput" onClick={this.inviteUser}>Invite user</button>
+                <button className="createProjectInput" onClick={this.emailValidationProcess}>Invite user</button>
                 <InviteList users={this.state.userList} />
                 <button className="createProjectInput" onClick={this.createProject}>Create project</button>
             </div>
