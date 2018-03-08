c5d8dc80547a640a2ecd25f34d00f3e96c10d40e
ericvon21 <evondra@purdue.edu>
2018-03-08 17:07:03 -0500

    Added text area and submit button for task comments

--- amora/src/ProjectDashboardComps/Task.js
+++ amora/src/ProjectDashboardComps/Task.js
@@ -390,6 +390,9 @@ class Task extends Component {
                         <div id="taskComments">
                             <Comment />
                             <Comment />
+                        <textarea rows="3" cols="50" >
+                            </textarea>
+                        <button type = "button">Comment</button>
                         </div>
                         <div className="closeTaskButton" onClick={this.switch}>~Close~</div>
                     </div>
