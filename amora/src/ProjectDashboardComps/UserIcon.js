2a800c944e401f89e4ab3d21e317ea168e4d99f2
David Wood <wood154@purdue.edu>
2018-03-25 16:55:43 -0400

    Today View able to be viewed by other users

--- amora/src/ProjectDashboardComps/UserIcon.js
+++ amora/src/ProjectDashboardComps/UserIcon.js
@@ -8,7 +8,9 @@ import 'react-responsive-modal/lib/react-responsive-modal.css';
 import Modal from 'react-responsive-modal/lib/css';
 
 import { App } from "../App.js"
-
+import MyDayTitleBar from '../MyDayComps/MyDayTitleBar.js';
+import TodayView from '../TodayView.js';
+import TodayViewUser from '../TodayViewUser.js';
 
 
 class UserIcon extends Component {
@@ -148,7 +150,8 @@ class UserIcon extends Component {
                     <div id="projectIndicator" style={{backgroundColor: this.color}}></div>
                         <Modal open={open} onClose={this.onCloseModal} little>
                           <h2>{this.state.displayName}<br/><br/>{this.state.email}</h2>
-                          <button onClick={this.removeUser}>Remove User from Project</button>
+                          <button onClick={this.removeUser}>Remove User from Project</button><br></br>
+                          <TodayViewUser uid={this.props.userID} getAppState={this.props.getAppState}/>
                         </Modal>
                 </div>
             )
@@ -165,7 +168,8 @@ class UserIcon extends Component {
                     {/*This should only appear if it is selected as the project*/}
                     <div id="projectIndicator" style={{backgroundColor: this.color}}></div>
                         <Modal open={open} onClose={this.onCloseModal} little>
-                          <h2>{this.state.displayName}<br/><br/>{this.state.email}</h2>
+                          <h2>{this.state.displayName}<br/><br/>{this.state.email}</h2><br></br>
+                          <TodayViewUser uid={this.props.userID} getAppState={this.props.getAppState}/>
                         </Modal>
                 </div>
             )
